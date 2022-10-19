f4076954c3ea07e3e0de797b060fc61b6befed1d
Punith Nandiraj <punithnandiraj@gmail.com>
2022-10-19 19:28:17 +0530

    form assignment initial design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import avatar from "./images/avatar.png";
+import { IoChevronBackCircleOutline } from "react-icons/io5";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-container">
+        <div className="icon">
+          <IoChevronBackCircleOutline />
+          <label> Back </label>
+        </div>
+        <div className="image-details">
+          <img src={avatar} alt="avatar" />
+        </div>
+        <div className="avatar-content">
+          <h5> Personal account </h5>
+          <h2> Andrew@polka.io </h2>
+        </div>
+        <div className="form-content">
+          <label> Password </label>
+          <form>
+            <input type="password" aria-label="Password" />
+            <h6>
+              {" "}
+              Forgot Password? <span> Reset </span>
+            </h6>
+          </form>
+        </div>
+        <div className="form-submit">
+          <div>
+            <input type="checkbox" id="sign-in" name="sign-in" checked />
+            <label for="sign-in"> Stay signed in </label>
+          </div>
+
+          <button> Sign in </button>
+        </div>
+
+        <div className="footer">
+          <hr />
+          <h6>
+            This site is protected by reCAPTCHA and the Google <br /> Privacy
+            Policy and Terms of Service apply.
+          </h6>
+        </div>
+      </div>
     </div>
   );
 }
